bc5e93b673d9179c9a7fc42a2a3de293714415f1
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-10-27 14:03:17 +0700

    Add Endpoint

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Book;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
@@ -26,7 +28,32 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $dataBuku = new Book;
+
+        $rules = [
+            'title' => 'required',
+            'author' => 'required',
+            'publication_date' => 'required|date'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json([
+                'status'=>false,
+                'massage'=>'Gagal memasukkan data',
+                'data'=>$validator->errors()
+            ]);
+        }
+
+        $dataBuku->title = $request->title;
+        $dataBuku->author = $request->author;
+        $dataBuku->publication_date = $request->publication_date;
+
+        $post = $dataBuku->save();
+
+        return response()->json([
+            'status'=> true,
+            'message' => 'Sukses memasukam data'
+        ]);
     }
 
     /**
@@ -55,7 +82,39 @@ class BookController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $dataBuku = Book::find($id);
+
+        if (empty($dataBuku)) {
+            return response()->json([
+                'status' => false,
+                'message'=> 'Data tidak ditemukan',
+            ], 404);
+        }
+
+        $rules = [
+            'title' => 'required',
+            'author' => 'required',
+            'publication_date' => 'required|date'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json([
+                'status'=>false,
+                'massage'=>'Gagal update data',
+                'data'=>$validator->errors()
+            ]);
+        }
+
+        $dataBuku->title = $request->title;
+        $dataBuku->author = $request->author;
+        $dataBuku->publication_date = $request->publication_date;
+
+        $post = $dataBuku->save();
+
+        return response()->json([
+            'status'=> true,
+            'message' => 'Sukses update data'
+        ]);
     }
 
     /**
@@ -63,6 +122,20 @@ class BookController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $dataBuku = Book::find($id);
+
+        if (empty($dataBuku)) {
+            return response()->json([
+                'status' => false,
+                'message'=> 'Data tidak ditemukan',
+            ], 404);
+        }
+
+        $post = $dataBuku->delete();
+
+        return response()->json([
+            'status'=> true,
+            'message' => 'Sukses delete data'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('buku', [BookController::class, 'index']);
-Route::get('buku/{id}', [BookController::class, 'show']);
+// Route::get('buku', [BookController::class, 'index']);
+// Route::get('buku/{id}', [BookController::class, 'show']);
+// Route::post('buku', [BookController::class, 'store']);
+// Route::put('buku/{id}', [BookController::class, 'update']);
+// Route::delete('buku/{id}', [BookController::class, 'destroy']);
+
+Route::apiResource('buku', BookController::class);
